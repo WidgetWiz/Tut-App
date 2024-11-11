56128b5f0a109618f50607855b171d80c1a1eea2
WidgetWiz <osamalala823@gmail.com>
2024-11-11 15:39:47 +0200

    mvvm intiating

---
+++ lib/presentation/base/baseviewmodel.dart
@@ -0,0 +1,11 @@
+abstract class BaseViewModel extends BaseViewModelInputs {
+  // shared variables and functions across view models
+}
+
+abstract class BaseViewModelInputs {
+  void start();
+  //body
+  void dispose();
+}
+
+abstract class BaseViewModelOutputs {}
